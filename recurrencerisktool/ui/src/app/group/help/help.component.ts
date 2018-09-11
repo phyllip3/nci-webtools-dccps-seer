fef5f00aeb9bb6bef318768f96c50fe983fdff8e
gfvalenzuela <gustavo.valenzuela@nih.gov>
2018-09-11 17:29:56 -0400

    Initial commit

---
+++ recurrencerisktool/ui/src/app/group/help/help.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'rrt-help',
+  templateUrl: './help.component.html',
+  styleUrls: ['./help.component.scss']
+})
+export class HelpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
